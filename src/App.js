8bacab61baf785b85812a7ea2d84596f0f70c591
Josmar Diaz <jdm.begreat28@gmail.com>
2022-07-12 20:18:06 -0400

    Created Component CreatePost and Instaniated it in App File

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 import DisplayPost from './Components/DisplayPost/DisplayPost';
-
-
+import CreatePost from './Components/CreatePost/CreatePost';
 
 function App() {
 
-
-const [entries, setEntries] = useState([{name: 'Josmar Diaz', post: 'This is my first post!'}, {name: 'Joey Martinez', post: 'There we go! Were getting somewhere!'}])
+  const [entries, setEntries] = useState([{name: 'Josmar Diaz', post: 'This is my first post!'}, {name: 'Joey Martinez', post: 'There we go! Were getting somewhere!'}])
   
+  function addNewPost(entry) {
+    let tempEntries = [entry, ...entries];
+    setEntries(tempEntries);
+  }
   
   return (
     <div>
+      <CreatePost addNewPostProperty={addNewPost} />
       <DisplayPost userEntries={entries} />
     </div>
   );
